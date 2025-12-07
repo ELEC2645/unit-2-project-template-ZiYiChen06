9a603686996b4bc3c4cd2dba61b849a7c59cd273
ZiYiChen06 <el23zc2@leeds.ac.uk>
2025-12-07 19:48:33 +0000

    code modify

--- funcs.c
+++ funcs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "funcs.h"
 
 void convert_frequency(void) {
@@ -13,9 +14,39 @@ void convert_temperature(void) {
 }
 
 void convert_dbm_mw(void) {
-    printf("\n[Power Conversion]\n");
-    printf("This feature will convert between dBm and mW.\n");
-    /* you can call a function from here that handles menu 3 */
+   int choice;
+   double value,result;
+
+   printf("\n[Power Conversion (dBm <-> mW)]\n");
+    printf("1.dBm -> mW \n");
+    printf("2.mW -> dBm \n");
+    printf("Enter your choice: ");
+   scanf("%d",&choice);
+
+   if(choice == 1) {
+       printf("Enter value in dBm: ");
+       if(scanf("%lf",&value)!=1) {
+           printf("Invalid input.\n");
+           return;
+       }
+       result = pow(10.0, value / 10.0);
+       printf("Result: %.2f dBm = %.4f mW\n", value, result);
+   }
+    else if(choice == 2) {
+       printf("Enter value in mW: ");
+       if (scanf("%lf",&value)!=1) {
+           printf("Invalid input.\n");
+           return;
+       }
+       if(value <= 0) {
+           printf("mW must be positive.\n");
+           return;
+       }
+       result = 10.0 * log10(value);
+       printf("Power in dBm: %.6f dBm\n", result);
+   } else {
+       printf("Invalid choice.\n");
+   }
 }
 
 void explanation_mode(void) {
